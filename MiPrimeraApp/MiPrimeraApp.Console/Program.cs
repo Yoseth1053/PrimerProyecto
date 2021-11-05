a842fdf0f21ce7aab6b55fc32d058c7b0502e2f7
yoseth <ywmateus@misena.edu.co>
2021-11-04 21:40:43 -0500

    ok

---
+++ MiPrimeraApp/MiPrimeraApp.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MiPrimeraApp.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenidos al 4 de noviembre");
+        }
+    }
+}
