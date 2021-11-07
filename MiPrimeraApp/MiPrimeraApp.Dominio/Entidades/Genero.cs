47a101062bda32fedbc132e4fa01c2598a583fe4
yoseth <ywmateus@misena.edu.co>
2021-11-07 13:44:31 -0500

    ok

---
+++ MiPrimeraApp/MiPrimeraApp.Dominio/Entidades/Genero.cs
@@ -0,0 +1,16 @@
+namespace MiPrimeraApp.Dominio
+{
+    /// <summary>
+    /// Lista Generos
+    /// </summary>
+    public enum Genero
+    {
+        Masculino,
+        femenino,
+        Bisexual,
+        Intersexsual,
+        homosexual,
+        Pansexual,
+        Transexual
+    }
+}
