898639c557ffcd6c80825ea7e84856a13afd733c
yoseth <ywmateus@misena.edu.co>
2021-11-07 21:16:37 -0500

    ok

--- MiPrimeraApp/MiPrimeraApp.Persistencia/AppRepositiorios/AppContext.cs
+++ MiPrimeraApp/MiPrimeraApp.Persistencia/AppRepositiorios/AppContext.cs
@@ -13,7 +13,9 @@ namespace MiPrimeraApp.Persistencia
         {
             if(!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseSqlServer("Inital catalog=HospitalAdsi2207208; DataSource=DESKTOP-G6077QO;  Integrated Security=true");
+                optionsBuilder.UseSqlServer("Inital catalog=HospitalAdsi2207208; DataSource=DESKTOP-G6077QO\SQLEXPRESS;  Integrated Security=true");
+                // .UseSqlServer(" DataSource = (localdb)\\DESKTOP-G6077QO; Inital catalog=HospitalAdsi2207208;");
+                
                 /*initial cataloh : Nombre de base de datos
                 source : nombre del servidor de la base de datos
                 integrated security: seguridad con la misma configuracion del inicio de secion*/
