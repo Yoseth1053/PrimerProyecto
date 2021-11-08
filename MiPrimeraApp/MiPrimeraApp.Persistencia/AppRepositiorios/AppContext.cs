be9eef2d8cfff5349260fa75797efc9e74a6f976
yoseth <ywmateus@misena.edu.co>
2021-11-07 20:36:06 -0500

    ok

--- MiPrimeraApp/MiPrimeraApp.Persistencia/AppRepositiorios/AppContext.cs
+++ MiPrimeraApp/MiPrimeraApp.Persistencia/AppRepositiorios/AppContext.cs
@@ -9,7 +9,17 @@ namespace MiPrimeraApp.Persistencia
         public DbSet<Paciente> Pacientes {get; set;}
         public DbSet<Medico> Medicos {get; set;}
         public DbSet<SignoVital> SignosVitales {get; set;}
-        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Inital catalog=HospitalAdsi2207208; DataSource=DESKTOP-G6077QO;  Integrated Security=true");
+                /*initial cataloh : Nombre de base de datos
+                source : nombre del servidor de la base de datos
+                integrated security: seguridad con la misma configuracion del inicio de secion*/
+
+            }
+        }
         
     }
 }
