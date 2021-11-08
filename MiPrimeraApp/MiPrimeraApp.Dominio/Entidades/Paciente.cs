cb41640232d7bca768ebaf61f93dd74ce2bdd026
yoseth <ywmateus@misena.edu.co>
2021-11-07 20:35:57 -0500

    ok

--- MiPrimeraApp/MiPrimeraApp.Dominio/Entidades/Paciente.cs
+++ MiPrimeraApp/MiPrimeraApp.Dominio/Entidades/Paciente.cs
@@ -10,17 +10,7 @@ namespace MiPrimeraApp.Dominio
         public DateTime FechaNacimiento {get; set;}
         public Medico Medico {get; set;}
         public List<SignoVital> SignosVitales {get; set;}
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if(!optionsBuilder.IsConfigured)
-            {
-                optionsBuilder.UseSqlServer("Inital catalog=HospitalAdsi2207208; DataSource=DESKTOP-G6077QO;  Integrated Security=true");
-                /*initial cataloh : Nombre de base de datos
-                source : nombre del servidor de la base de datos
-                integrated security: seguridad con la misma configuracion del inicio de secion*/
-
-            }
-        }
+        
 
         
     }
